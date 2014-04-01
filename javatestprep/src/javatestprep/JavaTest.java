aac3f8ec3894e2481ad1330f71f421fb4dbd20f7
Merrick <merrick.richmond@gmail.com>
2014-04-01 10:29:33 -0700

    First commit to remote repository. Should get a garbage file back too.
    Get gittin's

---
+++ javatestprep/src/javatestprep/JavaTest.java
@@ -0,0 +1,31 @@
+package javatestprep;
+
+public class JavaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1. promoting: byte, short, char, int, long; float & double
+		//   Note: You can't specify byte literal
+		tip.testIntegerPromotion();
+	}
+
+}
+class tip{
+	static byte b;
+	static short s;
+	static char c;
+	static int i;
+	static long l;
+	static void testIntegerPromotion(){
+		byte b=(byte) 0.5,b1=1,d,e=-128;
+		short s=0, s1=32100,s3;
+		char c=1,c2=65535;
+		o.p(Byte.toString(b));
+		o.p(""+tip.b);
+	}	
+}
+class o {
+	public static void p(String s){
+		System.out.println(s);
+	}
+}
